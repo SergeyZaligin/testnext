80eaafe54f5ba4d71e2ce04eda8449f2282999c7
sergey <sergey>
2018-07-18 12:59:27 +0300

    fix

--- client/layouts/default.vue
+++ client/layouts/default.vue
@@ -1,32 +1,45 @@
 <template>
-  <div>
-    <p v-if="us">Login {{ us }}</p>
-    <nav>
-      <ul>
-         <li>
-          <nuxt-link to="/">Home</nuxt-link>
-        </li>
-        <li>
-          <nuxt-link to="/login">Login</nuxt-link>
-        </li>
-         <li>
-          <nuxt-link to="/about">About</nuxt-link>
-        </li>
-        <li>
-          <nuxt-link to="/registration">Registration</nuxt-link>
-        </li>
-        <li>
-          <nuxt-link to="/admin">Admin</nuxt-link>
-        </li>
-        <li>
-          <nuxt-link to="/articles">Articles</nuxt-link>
-        </li>
-        <li>
-          <a @click="logout($event)" href="#">Logout</a>
-        </li>
-      </ul>
-    </nav>
-    <nuxt/>
+  <div id="wrapp">
+    <header class="header">
+      <div class="container">
+        <p v-if="us">Login {{ us }}</p>
+        <nav>
+          <ul>
+            <li>
+              <nuxt-link to="/">Home</nuxt-link>
+            </li>
+            <li>
+              <nuxt-link to="/login">Login</nuxt-link>
+            </li>
+            <li>
+              <nuxt-link to="/about">About</nuxt-link>
+            </li>
+            <li>
+              <nuxt-link to="/registration">Registration</nuxt-link>
+            </li>
+            <li>
+              <nuxt-link to="/admin">Admin</nuxt-link>
+            </li>
+            <li>
+              <nuxt-link to="/articles">Articles</nuxt-link>
+            </li>
+            <li>
+              <a @click="logout($event)" href="#">Logout</a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+    <main class="content" role="main">
+      <div class="container">
+        <nuxt/>
+      </div>
+    </main>
+    <footer class="footer">
+      <div class="container">
+        @ FOOTER
+      </div>
+    </footer>
   </div>
 </template>
 
