17f662fb2ca50f53685f65b3a31ba0ea86ddbc03
sergey <sergey>
2018-07-20 09:15:26 +0300

    fix

--- models/Agencys/Agency.js
+++ models/Agencys/Agency.js
@@ -1,8 +1,42 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
-const agencySchema = new Schema({
+const reviewSchema = new Schema({
+    visible: {
+        type: Boolean,
+        default: 1
+    },
+    user: {
+        ref: 'users',
+        type: Schema.Types.ObjectId
+    },
+    text: {
+        type: String,
+        required: true
 
+    }
+},
+{
+    timestamps: { 
+        createdAt: 'created_at', 
+        updatedAt: 'updated_at' 
+    }
+}
+);
+
+const agencySchema = new Schema({
+    visible: {
+        type: Boolean,
+        default: 1
+    },
+    description: {
+        type: String,
+        default: ''
+    },
+    keywords: {
+        type: String,
+        default: ''
+    },
     name: {
         type: String,
         required: true
@@ -12,10 +46,65 @@ const agencySchema = new Schema({
         unique: true,
         required: true
     },
+    site: {
+        type: String,
+        default: '',
+        required: true
+    },
+    phone: {
+        type: String,
+        default: '',
+        required: true
+    },
+    metro: {
+        type: String,
+        default: '',
+        required: true
+    },
+    address: {
+        type: String,
+        default: '',
+        required: true
+    },
+    email: {
+        type: String,
+        default: '',
+        required: true
+    },
+    preview: {
+        type: String,
+        default: ''
+    },
+    imageThubnailLogo: {
+        type: String,
+        default: ''
+    },
+    text: {
+        type: String,
+        default: ''
+    },
+    user: {
+        ref: 'users',
+        type: Schema.Types.ObjectId
+    },
+    sort: {
+        type: Number,
+        default: 1
+    },
     district: {
         ref: 'districts',
         type: String,
+    },
+    reviews: {
+        type: [reviewSchema]
     }
-})
+},
+{
+    timestamps: { 
+        createdAt: 'created_at', 
+        updatedAt: 'updated_at' 
+    }
+}
+)
 
 module.exports = mongoose.model('agencys',  agencySchema)
--- models/Agencys/District.js
+++ models/Agencys/District.js
@@ -2,6 +2,18 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const districtSchema = new Schema({
+    visible: {
+        type: Boolean,
+        default: 1
+    },
+    description: {
+        type: String,
+        default: ''
+    },
+    keywords: {
+        type: String,
+        default: ''
+    },
     name: {
         type: String
     },
@@ -9,8 +21,19 @@ const districtSchema = new Schema({
         type: String,
         unique: true,
         required: true
+    },
+    sort: {
+        type: Number,
+        default: 1
+    },
+},
+{
+    timestamps: { 
+      createdAt: 'created_at', 
+      updatedAt: 'updated_at' 
     }
-})
+}
+)
 
 
 //module.exports = mongoose.model('citys', citySchema);
